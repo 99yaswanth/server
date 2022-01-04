0bbfcfc5ad2045471d4f410070e1a9fe85dc74dc
99yaswanth <yashwanth24k@gmail.com>
2022-01-05 00:48:03 +0530

    deploy

---
+++ Jenkinsfile_deploy.groovy
@@ -0,0 +1,32 @@
+pipeline{
+    agent any
+    
+    parameters { 
+        string(name: 'SERVERIPS', defaultValue: '', description: '') 
+        //string(name: 'BRANCH', defaultValue: '', description: '')
+        string(name: 'BUILD', defaultValue: '', description: '')
+    }
+    stages{
+        stage("Multiple servers")
+        {
+            steps {
+                sh '''
+                    aws s3 cp s3://yashwanth24/hello-${BUILD}.war .
+                    ls -l
+                   inputArray=$SERVERIPS
+                   echo $inputArray
+                   IFS=',' read -r -a outputArray <<< "$inputArray"
+                   for ip in ${outputArray[@]}
+                   do
+                   echo "server ip is : $ip"
+                   done
+
+                 scp -o StrictHostKeyChecking=no -i /tmp/nvirginia1.pem target/hello-*.war ec2-user@$ip:/tmp/
+                 ssh -i /tmp/nvirginia1.pem ec2-user@$ip "sudo cp /tmp/hello-*.war /var/lib/tomcat/webapps"
+                   
+                   
+                   '''
+            }
+        }
+    }
+}
