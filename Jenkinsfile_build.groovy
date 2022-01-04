b42c6d966f6689cdf8c8e611844455cefa4f722f
99yaswanth <yashwanth24k@gmail.com>
2022-01-05 00:44:34 +0530

    s3

--- Jenkinsfile_build.groovy
+++ Jenkinsfile_build.groovy
@@ -21,8 +21,7 @@ pipeline{
         stage("uploading artifacts to s3"){
             steps{
                 println "upload artifacts to s3 bucket"
-                sh "echo $BUILD_NUMBER"
-                sh "aws s3 cp target/hello-${BUILD_NUMBER}.war s3://yashwanth12/"
+                sh "aws s3 cp target/hello-${BUILD_NUMBER}.war s3://yashwanth24/"
 
             }
         }
