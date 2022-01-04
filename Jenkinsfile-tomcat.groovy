b668d04f55b4c564b177a57610e9c728d65ef18a
99yaswanth <yashwanth24k@gmail.com>
2022-01-04 23:43:13 +0530

    tomcat file

---
+++ Jenkinsfile-tomcat.groovy
@@ -0,0 +1,16 @@
+pipeline{
+    agent any
+    parameters{
+        string(name: 'Serverip', defaultValue: '', description: '')
+    }
+    stages{
+        stage("tomcat installations"){
+            steps{
+                sh """
+                scp -o StrictHostKeyChecking=no -i /tmp/nvirginia1.pem /tmp/tomcatinstallation.sh ec2-user@${serverip}:/tmp
+                ssh -i /tmp/nvirginnia1.pem ec2-user@${serverip} "sudo bash /tmp/tomcatinstallation.sh"
+                  """
+            }
+        }
+    }
+}
