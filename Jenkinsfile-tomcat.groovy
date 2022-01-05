73043bbe4c3ecc32e304912a31d01a2bbf1e3505
99yaswanth <yashwanth24k@gmail.com>
2022-01-05 09:35:15 +0530

    updated tom

--- Jenkinsfile-tomcat.groovy
+++ Jenkinsfile-tomcat.groovy
@@ -6,10 +6,17 @@ pipeline{
     stages{
         stage("tomcat installations"){
             steps{
-                sh """
-                scp -o StrictHostKeyChecking=no -i /tmp/nvirginia1.pem /tmp/tomcatinstallation.sh ec2-user@${serverip}:/tmp
-                ssh -o StrictHostKeyChecking=no -i /tmp/nvirginia1.pem ec2-user@${serverip} "sudo bash /tmp/tomcatinstallation.sh"
-                  """
+                sh '''
+                inputArray=$Serverip
+                   echo $inputArray
+                   IFS=',' read -r -a outputArray <<< "$inputArray"
+                   for ip in ${outputArray[@]}
+                   do
+                   echo "server ip is : $ip"
+                   done
+                scp -o StrictHostKeyChecking=no -i /tmp/nvirginia1.pem /tmp/tomcatinstallation.sh ec2-user@$ip:/tmp
+                ssh -o StrictHostKeyChecking=no -i /tmp/nvirginia1.pem ec2-user@$ip "sudo bash /tmp/tomcatinstallation.sh"
+                  '''
             }
         }
     }
