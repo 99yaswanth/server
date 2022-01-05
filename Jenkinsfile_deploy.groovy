939fd9d6371e1803e680309b40e049c04ea2ab40
99yaswanth <yashwanth24k@gmail.com>
2022-01-05 08:05:17 +0530

    updated deploy file

--- Jenkinsfile_deploy.groovy
+++ Jenkinsfile_deploy.groovy
@@ -4,14 +4,14 @@ pipeline{
     parameters { 
         string(name: 'SERVERIPS', defaultValue: '', description: '') 
         //string(name: 'BRANCH', defaultValue: '', description: '')
-        string(name: 'BUILD', defaultValue: '', description: '')
+        //string(name: 'BUILD', defaultValue: '', description: '')
     }
     stages{
         stage("Multiple servers")
         {
             steps {
                 sh '''
-                    aws s3 cp s3://yashwanth24/hello-${BUILD}.war .
+                    
                     ls -l
                    inputArray=$SERVERIPS
                    echo $inputArray
@@ -21,8 +21,8 @@ pipeline{
                    echo "server ip is : $ip"
                    done
 
-                 scp -o StrictHostKeyChecking=no -i /tmp/nvirginia1.pem hello-${BUILD}.war ec2-user@$ip:/tmp/
-                 ssh -o StrictHostKeyChecking=no -i /tmp/nvirginia1.pem ec2-user@$ip "sudo cp /tmp/hello-${BUILD}.war /var/lib/tomcat/webapps"
+                 scp -o StrictHostKeyChecking=no -i /tmp/nvirginia1.pem target/hello-*.war ec2-user@$ip:/tmp/
+                 ssh -o StrictHostKeyChecking=no -i /tmp/nvirginia1.pem ec2-user@$ip "sudo cp /tmp/hello-*.war /var/lib/tomcat/webapps"
                    
   
                    '''
